8aebeb054da5634b6b584e3bee2f74aa99d4ac6a
JPantana <joshpantana@me.com>
2019-03-26 16:14:36 -0500

    added screenshot and updated readme

--- main.js
+++ main.js
@@ -66,7 +66,23 @@ const domStringBuilder = (x) => {
         domString += `<div class="row">`;
     x.forEach((product) => {
         domString +=    `<div class="col-4 cards">`;
-        domString +=        `<h2>${product.title}</h2>`;
+        domString +=        `<div class='card'>`;
+        domString +=            `<div class="card-header">${product.title}</div>`;
+        domString +=            `<img class="imgLoop" src="${product.imageUrl}" alt="">`;
+        domString +=    `<div class="card-body">`;
+        domString +=   `<h5 class="card-title">Card title</h5>`;
+        domString +=    `<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>`;
+        domString +=    `</div>`;
+        domString +=    `</div>`;
+
+        // domString +=        `<p>${product.description}<p>`;
+        // domString +=        `<img class="imgLoop" src="${product.imageUrl}" alt="">`;
+        // domString +=        `<p>${product.size}</p>`;
+        // domString +=        `<p>${product.weight}</p>`;
+        // domString +=        `<p>${product.price1}</p>`;
+        // domString +=        `<p>${product.price2}</p>`;
+        // domString +=        `<p>${product.price3}</p>`;
+        // domString +=        `<p>${product.validUntil}</p>`;
         domString +=    `</div>`;
         })
         domString += `</div>`;
